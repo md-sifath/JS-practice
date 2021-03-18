234da068c99bfa00bf030a6e07ff00d9cef80dfd
sifat <mdsifat12556@gmail.com>
2021-03-18 07:18:30 +0600

    complete task

--- challenge.js
+++ challenge.js
@@ -1,3 +1,99 @@
+/**************************** 
+var jhonmass,jhonheight,markmass,maskheight;
+jhonmass=30;
+jhonheight=1.5;
+markmass=28;
+markheight=1.2;
+var jhonBMI=jhonmass/(jhonheight*jhonheight);
+var markBMI=markmass/(markheight*markheight);
+console.log(jhonBMI);
+console.log(markBMI);
+var maxBMI=jhonBMI<markBMI;
+if(markBMI > jhonBMI){
+    console.log('Mark\'s BMI is Heigher than mark\'s BMI');
+}
+else{
+    console.log('Mark\'s BMI is lower than mark\'s BMI');
+}
+
+var jhonteamscore=80+120+100;
+var miketeamscore=80+120+100;
+var jhonavg=jhonteamscore/3;
+var mikeavg=miketeamscore/3;
+if(mikeavg>jhonavg){
+    console.log('mike team is winner');
+}
+else if(mikeavg<jhonavg){
+    console.log('jhon team is winwer');
+}
+else {
+    console.log('Draw Match');
+}
+var maryteamscore=80+120+100;
+var maryavg=maryteamscore/3;
+if(maryavg > (jhonavg && mikeavg)){
+    console.log('Mary is win');
+}
+else if(jhonavg > (maryavg && mikeavg)){
+    console.log('Jhon team is win');
+}
+else if(mikeavg> (maryavg && jhonavg)){
+    console.log('MIke team is win');
+}
+else{
+    console.log('Draw match');
+}
+/*****************************
+         Function
+
+function sumofnumber(x,y){
+    var totalsum=x+y;
+    return totalsum;
+}
+
+var ans=sumofnumber(10,20);
+console.log('The sum is '+ans);
+
+/*********************************
+ *       Arrays
+
+
+var names= ['ss', 'tt', 'aa',false,1990];
+console.log(names[1]);
+console.log(names);
+
+names.push('blue');
+console.log(names);
+names.unshift('that');
+console.log(names);
+names
+*/
+//***************************
+//Object and Properties
+
+
+var myfun= function(a,b){
+    return a+b;
+}
+var sum=myfun(10,20);
+console.log('the sum of 2 number is '+ sum);
+// Filter = return the whole value which is satisfi the condition\
+const price= [100, 200,300,400,560,600];
+
+const newprice= price.filter((elem, index ) => {
+    return elem >200;
+});
+console.log(newprice);
+
+
+// How to sort ar array  
+
+//Array.prototype.sort()
+
+const month=['june','feb','march','july','april'];
+const newmonth=month.splice(4,0,'dec');
+console.log(newmonth);
+
 
 //Codding Challenge 3//////
 /********************** 
